c727efd29ed76ced57fa4e751cec63cc37985651
szababurin <s.zababurin.v@gmail.com>
2021-08-04 03:02:40 +0300

    init

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    FILE *fp = fopen("index.js", "r");
+
+    char c;
+    while((c = fgetc(fp)) != EOF)
+        printf("%c", c);
+
+    fclose(fp);
+
+    printf("\n");
+
+    return 0;
+}
